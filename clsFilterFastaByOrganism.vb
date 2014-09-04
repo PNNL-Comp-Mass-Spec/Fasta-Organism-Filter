607625124568552f8950b04e1b4518920ab598e0
Matthew Monroe <matthew.monroe@pnnl.gov>
2014-09-04 20:28:45 +0000

    Initial commit

---
+++ clsFilterFastaByOrganism.vb
@@ -0,0 +1,395 @@
+Option Strict On
+
+' This class reads a fasta file and finds the organism info defined in the protein description lines
+' It optionally creates a filtered fasta file containing only the proteins of interest
+'
+' -------------------------------------------------------------------------------
+' Written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA)
+' Program started September 4, 2014
+' Copyright 2014, Battelle Memorial Institute.  All Rights Reserved.
+
+' E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com
+' Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/
+' -------------------------------------------------------------------------------
+'
+' Licensed under the Apache License, Version 2.0; you may not use this file except
+' in compliance with the License.  You may obtain a copy of the License at 
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+' Notice: This computer software was prepared by Battelle Memorial Institute, 
+' hereinafter the Contractor, under Contract No. DE-AC05-76RL0 1830 with the 
+' Department of Energy (DOE).  All rights in the computer software are reserved 
+' by DOE on behalf of the United States Government and the Contractor as 
+' provided in the Contract.  NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY 
+' WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS 
+' SOFTWARE.  This notice including this sentence must appear on any copies of 
+' this computer software.
+Imports System.IO
+Imports System.Runtime.InteropServices
+Imports System.Text.RegularExpressions
+
+Public Class clsFilterFastaByOrganism
+
+#Region "Constants and Enums"
+
+#End Region
+
+#Region "Structures"
+
+
+#End Region
+
+#Region "Classwide Variables"
+
+	
+#End Region
+
+#Region "Properties"
+
+	Public Property CreateProteinToOrganismMapFile As Boolean
+#End Region
+
+	Private Function ExtractOrganism(ByVal proteinDescription As String) As String
+
+		' Search for the organism info, assuming it is the last text seen between two square brackets
+		' However, there are exceptions we have to consider, for example
+		' [Salmonella enterica subsp. enterica serovar 4,[5],12:i:-]
+
+		Dim indexEnd As Integer = proteinDescription.LastIndexOf("]"c)
+		If indexEnd >= 0 Then
+			' Back track until we find [
+			' But, watch for ] while back-tracking
+			Dim subLevel = 1
+
+			For index = indexEnd - 1 To 0 Step -1
+				Dim chChar = proteinDescription.Substring(index, 1)
+				If chChar = "]" Then
+					subLevel += 1
+				ElseIf chChar = "[" Then
+					subLevel -= 1
+					If subLevel = 0 Then
+						Dim organism = proteinDescription.Substring(index + 1, indexEnd - index - 1)
+						Return organism
+					End If
+				End If
+			Next
+
+			Return proteinDescription.Substring(0, indexEnd - 1)			
+
+		End If
+
+		Return String.Empty
+
+	End Function
+
+	Public Function FilterFastaByOrganism(ByVal inputFilePath As String, ByVal organismListFile As String, ByVal outputFolderPath As String) As Boolean
+
+		Try
+
+			Dim oReader = New ProteinFileReader.FastaFileReader()
+			Dim intProteinsProcessed = 0
+
+			Dim diOutputFolder As DirectoryInfo = Nothing
+			If Not ValidateInputAndOutputFolders(inputFilePath, outputFolderPath, diOutputFolder) Then
+				Return False
+			End If
+
+			If String.IsNullOrWhiteSpace(organismListFile) Then
+				ShowErrorMessage("Organism list file not defined")
+				Return False
+			End If
+
+			Dim fiOrganismListFile = New FileInfo(organismListFile)
+			If Not fiOrganismListFile.Exists Then
+				ShowErrorMessage("Organism list file not found: " & fiOrganismListFile.FullName)
+				Return False
+			End If
+
+			ShowMessage("Loading the organism name filters from " & fiOrganismListFile.Name)
+
+			Dim lstTextFilters As List(Of String) = Nothing
+			Dim lstRegExFilters As List(Of Regex) = Nothing
+
+			If Not ReadOrganismFilterFile(fiOrganismListFile, lstTextFilters, lstRegExFilters) Then
+				Return False
+			End If
+
+			If lstTextFilters.Count = 0 AndAlso lstRegExFilters.Count = 0 Then
+				ShowErrorMessage("Organism list file is empty: " & fiOrganismListFile.FullName)
+				Return False
+			End If
+
+
+			If Not oReader.OpenFile(inputFilePath) Then
+				ShowErrorMessage("Error opening the fasta file; aborting")
+				Return False
+			End If
+
+			ShowMessage("Parsing " & Path.GetFileName(inputFilePath))
+
+			Dim baseName = Path.GetFileNameWithoutExtension(inputFilePath)
+			Dim filteredFastaFilePath = Path.Combine(diOutputFolder.FullName, baseName & "_Filtered.fasta")
+
+			ShowMessage("Creating " & Path.GetFileName(filteredFastaFilePath))
+
+			Using swFilteredFasta = New StreamWriter(New FileStream(filteredFastaFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+
+				While oReader.ReadNextProteinEntry()
+
+					Dim organism = ExtractOrganism(oReader.ProteinDescription)
+
+					Dim keepProtein = False
+
+					If lstTextFilters.Contains(organism) Then
+						keepProtein = True
+					Else
+						For Each regExSpec In lstRegExFilters
+							If regExSpec.IsMatch(oReader.ProteinDescription) Then
+								keepProtein = True
+								Exit For
+							End If
+						Next
+					End If
+
+					If keepProtein Then
+						WriteFastaFileEntry(swFilteredFasta, oReader)
+					End If
+
+					intProteinsProcessed += 1
+					If intProteinsProcessed Mod 50000 = 0 Then
+						ReportProgress("Working: " & oReader.PercentFileProcessed & "% fasta")
+					End If
+
+				End While
+
+			End Using
+
+		Catch ex As Exception
+			ShowErrorMessage("Error in FindOrganismsInFasta: " & ex.Message)
+			Return False
+		End Try
+
+		Return True
+
+
+	End Function
+
+	Public Function FindOrganismsInFasta(ByVal inputFilePath As String, ByVal outputFolderPath As String) As Boolean
+
+		Try
+
+			Dim oReader = New ProteinFileReader.FastaFileReader()
+			Dim intProteinsProcessed = 0
+
+			Dim diOutputFolder As DirectoryInfo = Nothing
+			If Not ValidateInputAndOutputFolders(inputFilePath, outputFolderPath, diOutputFolder) Then
+				Return False
+			End If
+
+			' Key is organism name, value is protein usage count			
+			Dim lstOrganisms = New Dictionary(Of String, Integer)
+
+			If Not oReader.OpenFile(inputFilePath) Then
+				ShowErrorMessage("Error opening the fasta file; aborting")
+				Return False
+			End If
+
+			ShowMessage("Parsing " & Path.GetFileName(inputFilePath))
+
+			Dim baseName = Path.GetFileNameWithoutExtension(inputFilePath)
+			Dim swMapFile As StreamWriter = Nothing
+			Dim mapFilePath As String = Path.Combine(diOutputFolder.FullName, baseName & "_ProteinOrganismMap.txt")
+
+			If Me.CreateProteinToOrganismMapFile Then
+				ShowMessage("Creating " & Path.GetFileName(mapFilePath))
+
+				swMapFile = New StreamWriter(New FileStream(mapFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+				swMapFile.WriteLine("Protein" & ControlChars.Tab & "Organism")
+			End If
+
+			While oReader.ReadNextProteinEntry()
+
+				Dim organism = ExtractOrganism(oReader.ProteinDescription)
+				If String.IsNullOrWhiteSpace(organism) Then
+					ShowMessage(" Warning: Organism not found for " & oReader.ProteinName)
+					Continue While
+				End If
+
+				Dim proteinCount As Integer
+				If lstOrganisms.TryGetValue(organism, proteinCount) Then
+					lstOrganisms(organism) = proteinCount + 1
+				Else
+					lstOrganisms.Add(organism, 1)
+				End If
+
+				If Not swMapFile Is Nothing Then
+					swMapFile.WriteLine(oReader.ProteinName & ControlChars.Tab & organism)
+				End If
+
+				intProteinsProcessed += 1
+				If intProteinsProcessed Mod 50000 = 0 Then
+					ReportProgress("Working: " & oReader.PercentFileProcessed & "% fasta")
+				End If
+
+			End While
+
+			If Not swMapFile Is Nothing Then
+				swMapFile.Close()
+			End If
+
+			Dim organismSummaryFilePath = Path.Combine(diOutputFolder.FullName, baseName & "_OrganismSummary.txt")
+
+			ShowMessage("Creating " & Path.GetFileName(organismSummaryFilePath))
+
+			' Now write out the unique list of organisms
+			Using swOrganismSummary = New StreamWriter(New FileStream(organismSummaryFilePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+				swOrganismSummary.WriteLine("Organism" & ControlChars.Tab & "Proteins")
+
+				Dim organismsSorted = (From item In lstOrganisms Select item Order By item.Key)
+
+				For Each organism In organismsSorted
+					swOrganismSummary.WriteLine(organism.Key & ControlChars.Tab & organism.Value)
+				Next
+
+			End Using
+			
+		Catch ex As Exception
+			ShowErrorMessage("Error in FindOrganismsInFasta: " & ex.Message)
+			Return False
+		End Try
+
+		Return True
+
+	End Function
+
+	Private Function ReadOrganismFilterFile(
+	  ByVal fiOrganismListFile As FileInfo,
+	  <Out()> ByVal lstTextFilters As List(Of String),
+	  <Out()> ByVal lstRegExFilters As List(Of Regex)) As Boolean
+
+		lstTextFilters = New List(Of String)(StringComparison.CurrentCultureIgnoreCase)
+		lstRegExFilters = New List(Of Regex)
+
+		Dim lineNumber = 0
+
+		Try
+			Using srOrganismFilterFile = New StreamReader(New FileStream(fiOrganismListFile.FullName, FileMode.Open, FileAccess.Read, FileShare.Read))
+
+				While srOrganismFilterFile.Peek > -1
+					Dim dataLine = srOrganismFilterFile.ReadLine()
+					lineNumber += 1
+
+					If String.IsNullOrWhiteSpace(dataLine) Then Continue While
+
+					' Check for "RegEx:"					
+					If dataLine.StartsWith("RegEx:") Then						
+						Dim regExFilter = dataLine.Substring("RegEx:".Length)
+						If String.IsNullOrWhiteSpace(regExFilter) Then
+							ShowMessage("  Warning: empty RegEx filter defined on line " & lineNumber)
+							Continue While
+						End If
+						lstRegExFilters.Add(New Regex(regExFilter, RegexOptions.Compiled Or RegexOptions.IgnoreCase))
+					Else
+						lstTextFilters.Add(dataLine)
+					End If
+
+				End While
+			End Using
+		Catch ex As Exception
+			ShowErrorMessage("Error in ReadOrganismFilterFile at line " & lineNumber & ": " & ex.Message)
+			Return False
+		End Try
+
+		Return True
+
+	End Function
+
+	Private Sub ReportProgress(ByVal strProgress As String)
+		Console.WriteLine(System.DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss") & " " & strProgress)
+	End Sub
+
+	Public Shared Sub ShowErrorMessage(ByVal strMessage As String)
+		Const strSeparator As String = "------------------------------------------------------------------------------"
+
+		Console.WriteLine()
+		Console.WriteLine(strSeparator)
+		Console.WriteLine(strMessage)
+		Console.WriteLine(strSeparator)
+		Console.WriteLine()
+
+	End Sub
+
+	Private Sub ShowMessage(ByVal message As String)
+		Console.WriteLine(message)
+	End Sub
+
+	Private Function ValidateInputAndOutputFolders(
+	  ByVal inputFilePath As String,
+	  ByRef outputFolderPath As String,
+	  <Out()> ByRef diOutputFolder As DirectoryInfo) As Boolean
+
+		Dim fiSourceFile = New FileInfo(inputFilePath)
+		If Not fiSourceFile.Exists Then
+			ShowErrorMessage("Source file not found: " & inputFilePath)
+			diOutputFolder = Nothing
+			Return False
+		End If
+
+		If String.IsNullOrWhiteSpace(outputFolderPath) Then
+			outputFolderPath = fiSourceFile.Directory.FullName
+		End If
+
+		diOutputFolder = ValidateOutputFolder(outputFolderPath)
+		If diOutputFolder Is Nothing Then Return False
+
+		If diOutputFolder.FullName <> fiSourceFile.Directory.FullName Then
+			ShowMessage("Output folder: " & diOutputFolder.FullName)
+		End If
+
+		Return True
+
+	End Function
+
+	Private Function ValidateOutputFolder(ByRef outputFolderPath As String) As DirectoryInfo
+		Try
+			If String.IsNullOrWhiteSpace(outputFolderPath) Then
+				outputFolderPath = "."
+			End If
+
+			Dim diOutputFolder = New DirectoryInfo(outputFolderPath)
+			If Not diOutputFolder.Exists Then
+				diOutputFolder.Create()
+			End If
+
+			Return diOutputFolder
+
+		Catch ex As Exception
+			ShowErrorMessage("Error validating the output folder: " & ex.Message)
+			Return Nothing
+		End Try
+
+	End Function
+
+	Private Sub WriteFastaFileEntry(ByRef swOutFile As StreamWriter, ByVal oReader As ProteinFileReader.FastaFileReader)
+
+		Const RESIDUES_PER_LINE As Integer = 60
+
+		swOutFile.WriteLine(oReader.HeaderLine)
+
+		' Now write out the residues
+		Dim intStartIndex = 0
+		Dim proteinSequence = oReader.ProteinSequence
+		Dim intLength = proteinSequence.Length
+
+		Do While intStartIndex < intLength
+			If intStartIndex + RESIDUES_PER_LINE <= intLength Then
+				swOutFile.WriteLine(proteinSequence.Substring(intStartIndex, RESIDUES_PER_LINE))
+			Else
+				swOutFile.WriteLine(proteinSequence.Substring(intStartIndex))
+			End If
+			intStartIndex += RESIDUES_PER_LINE
+		Loop
+
+	End Sub
+
+End Class
---
+++ modMain.vb
@@ -0,0 +1,177 @@
+﻿' This program reads a fasta file and finds the organism info defined in the protein description lines
+' It optionally creates a filtered fasta file containing only the proteins of interest
+'
+' -------------------------------------------------------------------------------
+' Written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA)
+' Program started September 4, 2014
+' Copyright 2014, Battelle Memorial Institute.  All Rights Reserved.
+
+' E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com
+' Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/
+' -------------------------------------------------------------------------------
+' 
+' Licensed under the Apache License, Version 2.0; you may not use this file except
+' in compliance with the License.  You may obtain a copy of the License at 
+' http://www.apache.org/licenses/LICENSE-2.0
+'
+Module modMain
+
+	Public Const PROGRAM_DATE As String = "September 4, 2014"
+
+	Private mInputFilePath As String
+	Private mOutputFolderPath As String
+	Private mOrganismListFile As String
+
+	Private mCreateProteinToOrganismMapFile As Boolean
+
+	Public Function Main() As Integer
+
+		Dim returnCode As Integer = 0
+
+		Try
+			' Set the default values
+			mInputFilePath = String.Empty
+
+			mOrganismListFile = String.Empty
+
+			Dim proceed = False
+			Dim oParseCommandLine As New clsParseCommandLine
+			If oParseCommandLine.ParseCommandLine Then
+				If SetOptionsUsingCommandLineParameters(oParseCommandLine) Then proceed = True
+			End If
+
+			If Not proceed OrElse
+			   oParseCommandLine.NeedToShowHelp OrElse
+			   oParseCommandLine.ParameterCount + oParseCommandLine.NonSwitchParameterCount = 0 OrElse
+			   mInputFilePath.Length = 0 Then
+				ShowProgramHelp()
+				returnCode = -1
+			Else
+				Dim oOrganismFilter = New clsFilterFastaByOrganism()
+
+				With oOrganismFilter
+
+					.CreateProteinToOrganismMapFile = mCreateProteinToOrganismMapFile
+
+					''If Not mParameterFilePath Is Nothing AndAlso mParameterFilePath.Length > 0 Then
+					''    .LoadParameterFileSettings(mParameterFilePath)
+					''End If
+				End With
+
+				Dim success = False
+				If String.IsNullOrEmpty(mOrganismListFile) Then
+					success = oOrganismFilter.FindOrganismsInFasta(mInputFilePath, mOutputFolderPath)
+				Else
+					success = oOrganismFilter.FilterFastaByOrganism(mInputFilePath, mOrganismListFile, mOutputFolderPath)
+				End If
+
+				If success Then
+					returnCode = 0
+				Else
+					returnCode = -1
+				End If
+
+			End If
+
+		Catch ex As System.Exception
+			clsFilterFastaByOrganism.ShowErrorMessage("Error occurred in modMain->Main: " & System.Environment.NewLine & ex.Message)
+			returnCode = -1
+		End Try
+
+		Return returnCode
+
+	End Function
+
+	Private Function GetAppVersion() As String
+		Return System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString() & " (" & PROGRAM_DATE & ")"
+	End Function
+
+	Private Function SetOptionsUsingCommandLineParameters(ByVal objParseCommandLine As clsParseCommandLine) As Boolean
+		' Returns True if no problems; otherwise, returns false
+
+		Dim strValue As String = String.Empty
+		Dim strValidParameters() As String = New String() {"I", "Org", "O", "Map"}
+
+		Try
+			' Make sure no invalid parameters are present
+			If objParseCommandLine.InvalidParametersPresent(strValidParameters) Then
+				Return False
+			Else
+
+				' Query objParseCommandLine to see if various parameters are present
+				With objParseCommandLine
+					If .RetrieveValueForParameter("I", strValue) Then
+						mInputFilePath = strValue
+					ElseIf .NonSwitchParameterCount > 0 Then
+						mInputFilePath = .RetrieveNonSwitchParameter(0)
+					End If
+
+					If .RetrieveValueForParameter("Org", strValue) Then
+						mOrganismListFile = strValue
+					End If
+
+					If .RetrieveValueForParameter("O", strValue) Then
+						mOutputFolderPath = strValue
+					End If
+
+					If .IsParameterPresent("Map") Then mCreateProteinToOrganismMapFile = True
+				End With
+
+				Return True
+			End If
+
+		Catch ex As Exception
+			clsFilterFastaByOrganism.ShowErrorMessage("Error parsing the command line parameters: " & System.Environment.NewLine & ex.Message)
+		End Try
+
+		Return False
+
+	End Function
+
+	Private Sub ShowProgramHelp()
+
+		Try
+
+			Console.WriteLine("This program reads a fasta file and finds the organism info defined in the protein description lines. " +
+			  "It optionally creates a filtered fasta file containing only the proteins of interest")
+			Console.WriteLine()
+
+			Console.WriteLine("Program syntax #1:" & ControlChars.NewLine & System.IO.Path.GetFileName(Reflection.Assembly.GetExecutingAssembly().Location) &
+			  " SourceFile.fasta [/O:OutputFolderPath] [/Map]")
+
+			Console.WriteLine("Program syntax #2:" & ControlChars.NewLine & System.IO.Path.GetFileName(Reflection.Assembly.GetExecutingAssembly().Location) &
+			  " SourceFile.fasta /Org:OrganismListFile.txt [/O:OutputFolderPath]")
+
+			Console.WriteLine()
+
+			Console.WriteLine("The input file name is required. If the filename contains spaces, then surround it with double quotes")
+			Console.WriteLine()
+			Console.WriteLine("For the first syntax, will find the organisms present in the fasta file, creating an OrganismSummary file")
+			Console.WriteLine("Assumes the organism name is defined by the last set of square brackets in the protein description")
+			Console.WriteLine("")
+			Console.WriteLine("Use /Map to also create a file mapping protein name to organism name, with filename SourceFasta_ProteinOrganismMap.txt")
+			Console.WriteLine()
+			Console.WriteLine("For the second syntax, use /Org to specify a text file listing organism names that should be used for filtering the fasta file")
+			Console.WriteLine("The program will create a new fasta file that only contains proteins from the organisms of interest")
+			Console.WriteLine()
+			Console.WriteLine("The OrganismListFile should have one organism name per line")
+			Console.WriteLine("Organism names that start with RegEx: will be treated as regular expressions for matching to protein descriptions")
+			Console.WriteLine("Otherwise, assumes that the protein name is the text betweeen the last set of square brackets in the protein description")
+			Console.WriteLine()
+			Console.WriteLine("For both modes, use /O to specify an output folder.  If /O is missing, the output files will be created in the same folder as the source fasta file")
+			Console.WriteLine()
+			Console.WriteLine("Program written by Matthew Monroe for the Department of Energy (PNNL, Richland, WA) in 2014")
+			Console.WriteLine("E-mail: matthew.monroe@pnnl.gov or matt@alchemistmatt.com")
+			Console.WriteLine("Website: http://omics.pnl.gov/ or http://www.sysbio.org/resources/staff/")
+			Console.WriteLine()
+
+			' Delay for 750 msec in case the user double clicked this file from within Windows Explorer (or started the program via a shortcut)
+			System.Threading.Thread.Sleep(750)
+
+		Catch ex As Exception
+			clsFilterFastaByOrganism.ShowErrorMessage("Error displaying the program syntax: " & ex.Message)
+		End Try
+
+	End Sub
+
+End Module
