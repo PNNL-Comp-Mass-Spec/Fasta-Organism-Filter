5688791356c41d1af5979d82fa6806de17bf12ff
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-16 16:21:39 -0800

    Whitespace update

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -2,34 +2,34 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
+' General Information about an assembly is controlled through the following
 ' set of attributes. Change these attribute values to modify the information
 ' associated with an assembly.
 
 ' Review the values of the assembly attributes
 
-<Assembly: AssemblyTitle("FastaOrganismFilter")> 
-<Assembly: AssemblyDescription("")> 
-<Assembly: AssemblyCompany("PNNL")> 
-<Assembly: AssemblyProduct("FastaOrganismFilter")> 
-<Assembly: AssemblyCopyright("Copyright © PNNL 2014")> 
-<Assembly: AssemblyTrademark("")> 
+<Assembly: AssemblyTitle("FastaOrganismFilter")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("PNNL")>
+<Assembly: AssemblyProduct("FastaOrganismFilter")>
+<Assembly: AssemblyCopyright("Copyright © PNNL 2014")>
+<Assembly: AssemblyTrademark("")>
 
 <Assembly: ComVisible(False)>
 
 'The following GUID is for the ID of the typelib if this project is exposed to COM
-<Assembly: Guid("15be75a9-b2fd-414c-b2f6-264e4d8d7125")> 
+<Assembly: Guid("15be75a9-b2fd-414c-b2f6-264e4d8d7125")>
 
 ' Version information for an assembly consists of the following four values:
 '
 '      Major Version
-'      Minor Version 
+'      Minor Version
 '      Build Number
 '      Revision
 '
-' You can specify all the values or you can default the Build and Revision Numbers 
+' You can specify all the values or you can default the Build and Revision Numbers
 ' by using the '*' as shown below:
-' <Assembly: AssemblyVersion("1.0.*")> 
+' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.*")> 
+<Assembly: AssemblyVersion("1.0.*")>
 
