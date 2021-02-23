ede2c693bd21d48c2e5d3d327897b9393c64d2b2
alchemistmatt <matthew.monroe@pnnl.gov>
2021-02-23 09:52:47 -0800

    Update the program date and bump the version

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -25,8 +25,5 @@ using System.Runtime.InteropServices;
 // Build Number
 // Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.1.*")]
